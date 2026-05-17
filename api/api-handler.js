dd67c64526ccaa63bd9184ac38fad810840a23f7
Suhail <suhail.business0001@gmail.com>
2026-05-17 10:53:46 +0530

    migrated serverless router to direct client side multi engine extraction cluster

--- api/api-handler.js
+++ api/api-handler.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-   export default async function handler(req, res) {
     // CORS கொள்கைகளுக்கான பாதுகாப்பு அமைப்புகள்
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
